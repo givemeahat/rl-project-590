2f58fa2acabee5adb284ceb54238011f9a489ee8
iigindesign <18wangs@gmail.com>
2021-11-30 11:27:10 -0500

    New discrete action scheme
    now has a branch of 3 actions to mimic get button down and up
    0: do absolutely nothing
    1: dive (add gravity)
    2: undive (delete gravity)

--- Assets/jankController.cs
+++ Assets/jankController.cs
@@ -74,7 +74,6 @@ public class jankController : MonoBehaviour
 
     public void dive(){
         m_Rigidbody2D.gravityScale = added_gravity;
-        Debug.Log("dived");
     }
 
     public void diveFalse(){
--- Assets/jankGent.cs
+++ Assets/jankGent.cs
@@ -36,24 +36,40 @@ public class jankGent : Agent
     public override void Heuristic(in ActionBuffers actionsOut)
     {
         ActionSegment<int> discreteActions = actionsOut.DiscreteActions;
+        // discreteActions[0] = 1;
+        /* bug is possibly happening because in heuristics it's assuming to have three actions somehow...
+        0 is do nothing; but in heuristics after diving somehow 
+
+        wait this makes kinda of sense ... why three branches work for heuristics but doesnt for the bot
+
+        cuz we can check for getbuttonup so gravity resets, but this cheat doesnt exist for bot
+        for bot it's just
+        2. RESTORES original gravity 
+        1. dive added gravity; once added gravity in one click, unless select do nothing immediately, gravit doesnt change 
+        0. do nothing (retains gravity)
+
+        */
         if(Input.GetButtonDown("Dive")){
             discreteActions[0] = 1;
-            Debug.Log("dive");
         }
         if(Input.GetButtonUp("Dive")){
-            discreteActions[0] = 0;
+            discreteActions[0] = 2;
         }
+
+        Debug.Log("passing" + discreteActions[0]);
+
     }
 
     public override void OnActionReceived(ActionBuffers actions)
     {
         int dive = actions.DiscreteActions[0];
-        if (Mathf.FloorToInt(dive) == 1){
+        Debug.Log("received" + dive);
+        if (dive == 1){
             controller.dive();
             diving = true;
 
         }
-        if (Mathf.FloorToInt(dive) == 0){
+        if (dive == 2){
             controller.diveFalse();
             diving = false;
         }
